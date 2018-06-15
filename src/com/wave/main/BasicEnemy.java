34ac268cdbadc15ac1db89bf909592d5ce140d2f
Eddy Martinez <eddymh@vt.edu>
2018-06-14 20:33:39 -0400

    add trail to enemy

--- src/com/wave/main/BasicEnemy.java
+++ src/com/wave/main/BasicEnemy.java
@@ -2,14 +2,23 @@ package com.wave.main;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 
 public class BasicEnemy extends GameObject {
+	
+	private Handler handler;
 
-	public BasicEnemy(int x, int y, ID id) {
+	public BasicEnemy(int x, int y, ID id, Handler handler) {
 		super(x, y, id);
+		
+		this.handler = handler;
 		
 		velX = 5;
 		velY = 5;
+	}
+	
+	public Rectangle getBounds() {
+		return new Rectangle(x, y, 16, 16);
 	}
 
 	public void tick() {
@@ -18,6 +27,8 @@ public class BasicEnemy extends GameObject {
 		
 		if(y <= 0 || y >= Game.HEIGHT - 32) velY *= -1;
 		if(x <= 0 || x >= Game.WIDTH - 16) velX *= -1;
+		
+		handler.addObject(new Trail(x,y, ID.Trail, 16, 16, Color.red, 0.02f, handler));
 	}
 
 	public void render(Graphics g) {
--- src/com/wave/main/ID.java
+++ src/com/wave/main/ID.java
@@ -3,5 +3,6 @@ package com.wave.main;
 public enum ID {
 
 	Player(),
-	BasicEnemy();
+	BasicEnemy(),
+	Trail();
 }
